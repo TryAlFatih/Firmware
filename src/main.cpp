fff03f496576b39c923831d992be03073316ce8e
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-07-25 17:07:50 +0700

    Menambahkan pin 2 sebagai Push Button (#5)

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(2, INPUT);
 }
 
 void loop() {
+  bool btn_state = digitalRead(2);
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(12, HIGH);
   delay(2000);

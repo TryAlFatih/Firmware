5aaf272e84ce220a442638b7c8a18af5851955c1
Hidayat Eko Saputro <hidayat.e.s@mail.ugm.ac.id>
2019-07-25 17:40:50 +0700

    Menambahkan fungsi bacaADC
    SDSDFD

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+float bacaADC(int pin);
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(12, OUTPUT);
@@ -14,4 +16,19 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(12, LOW);
   delay(2000);
+}
+
+
+
+/**
+ * @brief baca fungsi ADC
+ * 
+ * @param pin input
+ * @return float data hasi konversi
+ */
+float bacaADC(int pin){
+  float data = analogRead(pin);
+  
+  data = 4/10*100;
+  return data;
 }

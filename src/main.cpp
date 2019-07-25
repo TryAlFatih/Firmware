0cd78811d7718687f07b6e7da426114316466304
billynugrahas <33423340+billynugrahas@users.noreply.github.com>
2019-07-25 17:43:29 +0700

    Menambahkan fungsi untuk mengakses lcd 16x2 (#10)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+void tampilLcd(int x, int y, char* fmtstr);
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(12, OUTPUT);
@@ -14,4 +16,17 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(12, LOW);
   delay(2000);
+}
+
+/**
+ * @brief Fungsi mengakses lcd 16x2 dari string yang dipilih
+ * 
+ * @param x Baris yang dipilih untuk menampilkan string 
+ * @param y Kolom yang dipilih untuk menampilkan string 
+ * @param fmtstr String yang akan ditampilkan
+ */
+
+void tampilLcd(int x, int y, char* fmtstr)
+{
+
 }

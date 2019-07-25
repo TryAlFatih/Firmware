07feaa125c3dbf9de7c70d69882ff03426cde72b
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-07-25 10:49:58 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(12, HIGH);
   delay(2000);
   digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(12, LOW);
   delay(2000);
 }
